15189c3dbbe6b60ac29f9f00c66679b6598f8c73
cyrus25 <cyruspassi@gmail.com>
2021-11-07 06:52:24 +0530

    added option for vscode extension publish

--- publish_version.js
+++ publish_version.js
@@ -1,6 +1,11 @@
 const packageJsonVersion = require('./package.json').version;
 const { spawn } = require("child_process");
 
+
+
+const project = process.argv[2].trim();
+
+
 if (packageJsonVersion.includes('rc')) {
     publishVersion('next');
 } else if (packageJsonVersion.includes('alpha')) {
@@ -10,7 +15,13 @@ if (packageJsonVersion.includes('rc')) {
 }
 
 function publishVersion(tag) {
-    var ls = spawn('yarn', ['publish', '--tag=' + tag, '--new-version=' + packageJsonVersion]);
+
+    let ls = null;
+    if (project === "vscode")
+        ls = spawn('npx', ['vsce', 'publish', packageJsonVersion]);
+    else
+        ls = spawn('yarn', ['publish', '--tag=' + tag, '--new-version=' + packageJsonVersion]);
+
     ls.stdout.on("data", function (data) {
         console.log(`${data}`);
     });
